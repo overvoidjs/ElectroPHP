fc9414c86698f99ecdd882a1c3a6aa92dae59d1f
HomeBook <loisoj@gmail.com>
2019-05-29 23:39:34 +0300

    highWay to fix

--- main.js
+++ main.js
@@ -1,141 +1,86 @@
 const electron = require('electron')
-// Module to control application life.
+// Главный контроллер приложения
 const app = electron.app
-// Module for mennu
-const Menu = electron.Menu
-// Module to create native browser window.
+
+// Создаем меню
+// const Menu = electron.Menu
+
+// Создаем окно браузера
 const BrowserWindow = electron.BrowserWindow
 
 const path = require('path')
 const url = require('url')
 
-/////////////////////////////
+// app.on('ready', () => {
+//   createWindow()
+// })
 
-///////////////////////////////
-// Copy paste fixed by this
-
-app.on('ready', () => {
-  createWindow()
-  if (process.platform === 'darwin') {
-    var template = [{
-      label: 'FromScratch',
-      submenu: [{
-        label: 'Quit',
-        accelerator: 'CmdOrCtrl+Q',
-        click: function() { app.quit(); }
-      }]
-    }, {
-      label: 'Edit',
-      submenu: [{
-        label: 'Undo',
-        accelerator: 'CmdOrCtrl+Z',
-        selector: 'undo:'
-      }, {
-        label: 'Redo',
-        accelerator: 'Shift+CmdOrCtrl+Z',
-        selector: 'redo:'
-      }, {
-        type: 'separator'
-      }, {
-        label: 'Cut',
-        accelerator: 'CmdOrCtrl+X',
-        selector: 'cut:'
-      }, {
-        label: 'Copy',
-        accelerator: 'CmdOrCtrl+C',
-        selector: 'copy:'
-      }, {
-        label: 'Paste',
-        accelerator: 'CmdOrCtrl+V',
-        selector: 'paste:'
-      }, {
-        label: 'Select All',
-        accelerator: 'CmdOrCtrl+A',
-        selector: 'selectAll:'
-      }]
-    }];
-    var osxMenu = Menu.buildFromTemplate(template);
-    Menu.setApplicationMenu(osxMenu);
-  }
-})
-
-// PHP SERVER CREATION /////
+// Создаем PHP сервер /////
 const PHPServer = require('php-server-manager');
 
 const server = new PHPServer({
     port: 7676,
     directory: __dirname,
     directives: {
-        display_errors: 1,
-        expose_php: 1
+        display_errors: 0,
+        expose_php: 0
     }
 });
 
-//////////////////////////
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+// Глобально обьявляем переменную главного окна
 let mainWindow
 
 function createWindow () {
 
   server.run();
-  // Create the browser window.
+  // Создаем окно браузера
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
-  // and load the index.html of the app.
+  // Слушаем запушенный сервер php
   mainWindow.loadURL('http://'+server.host+':'+server.port+'/')
 
-/*
+/* Это используем только с index.html
+
 mainWindow.loadURL(url.format({
   pathname: path.join(__dirname, 'index.php'),
   protocol: 'file:',
   slashes: true
 }))
 */
- const {shell} = require('electron')
+ // const {shell} = require('electron')
  // shell.showItemInFolder('fullPath')
 
-  // Open the DevTools.
+  // Включить DeBug.
   // mainWindow.webContents.openDevTools()
 
-  // Emitted when the window is closed.
+  // Event при закрытии окна
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    // PHP SERVER QUIT
+    // Закрываем php сервер
     server.close();
     mainWindow = null;
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+// Этот метод будет вызван, когда Electron отработает
+// Проинициализируется и будет готов к созданию окон браузера.
+// Некоторые API можно использовать только после этого события.
 
-// app.on('ready', createWindow)
+app.on('ready', createWindow)
 
-// Quit when all windows are closed.
+// Выходим если все окна были закрыты
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    // PHP SERVER QUIT
+  // Киляем PHP Server
+
+  // if (process.platform !== 'darwin') {
     server.close();
     app.quit();
-  }
+  // }
 })
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
+//Если приложение активно, но окно упало, запускаем повторно
   if (mainWindow === null) {
     createWindow()
   }
 })
-
-
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
